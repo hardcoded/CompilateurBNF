52c2d7f1438ed929ef0181bf6843d0832990969d
JohanBrunet <brunet.johan.23@gmail.com>
2016-06-02 12:36:09 +0200

    Add color to the Vertex structure

--- Compilateur/src/colorgraph/Vertex.java
+++ Compilateur/src/colorgraph/Vertex.java
@@ -2,9 +2,27 @@ package colorgraph;
 
 public class Vertex {
 
-	String value;
+	private String value;
+	private int color;
 	
 	public Vertex(String val) {
 		this.value = val;
+		this.color = -1;
 	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}	
 }
